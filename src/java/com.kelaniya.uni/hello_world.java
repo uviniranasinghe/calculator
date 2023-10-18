e0ded97d789d2df8d9db799886b53595f78677fd
uviniranasinghe <139993670+uviniranasinghe@users.noreply.github.com>
2023-10-18 17:29:58 +0530

    innitial commit

---
+++ src/java/com.kelaniya.uni/hello_world.java
@@ -0,0 +1,9 @@
+package com.kelaniya.uni;
+
+import java.util.Arrays;
+
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello world" );
+    }
+}
